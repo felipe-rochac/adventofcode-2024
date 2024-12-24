b6fccb8273c067b8c88c7b32d9a791564f87ca88
Felipe Rocha Correa <15219102+felipe-rochac@users.noreply.github.com>
2024-12-24 12:42:35 -0500

    changes

--- main.go
+++ main.go
@@ -1,32 +1,13 @@
 package main
 
 import (
-	day "adventofcode-2025/day2"
+	day "adventofcode-2024/day2"
 	"encoding/base64"
 	"fmt"
 	"log"
 )
 
-func decodeBase64() {
-	decimalValues := []byte{
-		50,
-		57,
-		182,
-		144,
-		98,
-		248,
-		128,
-		71,
-		135,
-		76,
-		41,
-		230,
-		158,
-		122,
-		219,
-		67,
-	}
-
+func decodeBase64(decimalValues []byte) {
 	// Convert the byte array to a Base64 string
 	base64String := base64.URLEncoding.EncodeToString(decimalValues)
 	fmt.Println("Base64 String:", base64String)
